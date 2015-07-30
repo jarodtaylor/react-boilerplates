5665df4913eac2923fa20a8cde7fe9c7fe4b939b
Jarod Taylor <jarod@wellmatchhealth.com>
2015-07-30 18:10:12 -0500

    added CSS dependencies, inline image handling, and some basic css4 styling to index

--- src/hello.js
+++ src/hello.js
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 export default class Hello extends Component {
   render() {
     return (
-      <h1>Hello, world!!</h1>
+      <div className="wrapper">
+        <image className="logo" src={require("./images/react_logo.svg")} />
+        <h1>Hello, world!</h1>
+        <p>This is just another React boilerplate. This boilerplate is as minimalist as can be, but includes everything you'd need to build your React JS project.</p>
+        <ul className="checklist">
+          <li><i className="fa fa-check"></i>React JS</li>
+          <li><i className="fa fa-check"></i>Webpack</li>
+          <li><i className="fa fa-check"></i>Babel</li>
+          <li><i className="fa fa-check"></i>cssnext</li>
+          <li><i className="fa fa-check"></i>PostCSS</li>
+          <li><i className="fa fa-check"></i>React Hot Loader</li>
+          <li><i className="fa fa-check"></i>ESLint</li>
+        </ul>
+      </div>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   devtool: 'eval',
@@ -13,18 +14,36 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/static/'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        // loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+        loader: "style!css?sourceMap!cssnext"
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        loader: 'file-loader?name=[name].[ext]'
+        // loader: 'url?limit=25000'
+      },
+      {
+        test: /\.ttf$/,
+        loader: 'url?limit=100000'
+      }
+
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    // new ExtractTextPlugin('bundle.css')
+  ]
 };
